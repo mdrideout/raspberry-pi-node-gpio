4f6cbdc9a7b1e50f067e321bcb28608a77d2a9f7
Matthew Rideout <boon4376@gmail.com>
2019-02-25 21:45:00 -0500

    Updated to build only on armhf. Updated connect script to remove blocked conditionals.

--- scripts/bin/connect-gpio.sh
+++ scripts/bin/connect-gpio.sh
@@ -3,15 +3,6 @@
 # Makes GPIO connections for raspberry pi
 echo "Starting GPIO connection process...";
 
-# If "pi" snap is installed, make our connection
-if $(snap list pi); then
-    echo "pi package installed, connecting gpio"
-    snap connect raspberry-pi-node-gpio:gpio pi:bcm-gpio-4
-fi
-
-if ! $(snap list pi); then
-    echo "pi package not installed. No GPIO to connect"
-fi
-
+snap connect raspberry-pi-node-gpio:gpio pi:bcm-gpio-4
 
 echo "GPIO connection process complete."
