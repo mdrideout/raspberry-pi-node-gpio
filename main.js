e7c53be15d06d72c509c16f618d2cc8c697732b7
Matthew Rideout <boon4376@gmail.com>
2019-03-03 22:50:59 -0500

    Nodejs script successfully reading the GPIO4 pin value

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+var fs = require("fs");
 
-console.log("This is a test message.");
+console.log("Reading value of pin gpio4...");
+
+fs.readFile('/sys/class/gpio/gpio4/value', 'utf8', (err, contents) => {
+    if(err) {
+        console.log("Error: ", err);
+    }
+    console.log("Value: ", contents);
+});
+
+console.log("Reading complete.");
